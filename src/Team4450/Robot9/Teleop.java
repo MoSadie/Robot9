8d6528615ac43c1c264551c597e35b5c0765448d
MoSadie <sean@cushmantech.net>
2016-02-05 14:38:11 -0800

    Fixed Naming of CANTalon variables

--- src/Team4450/Robot9/Teleop.java
+++ src/Team4450/Robot9/Teleop.java
@@ -102,7 +102,7 @@ class Teleop
         // Motor safety turned on.
         robot.robotDrive.setSafetyEnabled(true);
         
-        robot.RFTalon.setEncPosition(0);
+        robot.right_front.setEncPosition(0);
         
 		// Driving loop runs until teleop is over.
 
@@ -125,7 +125,7 @@ class Teleop
 
 			//LCD.printLine(7, "penc=%d  pv=%d", robot.RFTalon.getPulseWidthPosition(), robot.RFTalon.getPulseWidthVelocity());
 			//LCD.printLine(8, "aenc=%d  av=%d", robot.RFTalon.getAnalogInPosition(), robot.RFTalon.getAnalogInVelocity());
-			LCD.printLine(9, "qenc=%d  qv=%d", robot.RFTalon.getEncPosition(), robot.RFTalon.getEncVelocity());
+			LCD.printLine(9, "qenc=%d  qv=%d", robot.right_front.getEncPosition(), robot.right_front.getEncVelocity());
 			
 			LCD.printLine(10, "A=%b  B=%b  pot=%d  potv=%f  hes=%b", revBoard.getButtonA(), revBoard.getButtonB(), 
 					revBoard.getPotValue(), revBoard.getPot().getVoltage(), false); //, !hallEffectSensor.get());
